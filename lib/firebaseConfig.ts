bef30796cf4cd4b042c72ccd2d098eedc3ff899b
whitem0824 <whitem0824@gmail.com>
2025-09-17 20:14:33 +0000

    feat: update firebase config directly

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,20 +1,22 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCvE-BO2puKDD6ZF0fz9IVCpMnLRqGD6n4",
+  authDomain: "tradesitegenie.firebaseapp.com",
+  databaseURL: "https://tradesitegenie-default-rtdb.firebaseio.com",
+  projectId: "tradesitegenie",
+  storageBucket: "tradesitegenie.firebasestorage.app",
+  messagingSenderId: "692679763264",
+  appId: "1:692679763264:web:25bad17a7b382b7554c6a5",
+  measurementId: "G-FHP4F9D318"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
